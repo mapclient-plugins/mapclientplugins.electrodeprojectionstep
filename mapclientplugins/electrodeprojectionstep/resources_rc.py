92fbf04ce485bf471097b4132461c272a94d843f
TimmyNic <tim.nicolas@outlook.com>
2023-01-19 17:27:36 +1300

    Upgrade to PySide6.

--- mapclientplugins/electrodeprojectionstep/resources_rc.py
+++ mapclientplugins/electrodeprojectionstep/resources_rc.py
@@ -1,9 +1,9 @@
 # Resource object code (Python 3)
 # Created by: object code
-# Created by: The Resource Compiler for Qt version 5.15.2
+# Created by: The Resource Compiler for Qt version 6.4.1
 # WARNING! All changes made in this file will be lost!
 
-from PySide2 import QtCore
+from PySide6 import QtCore
 
 qt_resource_data = b"\
 \x00\x00\x0d\x08\
@@ -243,7 +243,7 @@ qt_resource_struct = b"\
 \x00\x00\x004\x00\x02\x00\x00\x00\x01\x00\x00\x00\x03\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00F\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01y\xce\xfc\xfc\xaf\
+\x00\x00\x01\x85\xc8Dd\x5c\
 "
 
 def qInitResources():
